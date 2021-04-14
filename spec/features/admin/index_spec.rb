79b2c5754ff541d7232d54f8bdb479a3148514a2
George Ho <george.h.w.ho@gmail.com>
2021-04-14 07:50:19 -0400

    Add admin dashboard tests

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 describe 'admin index page' do
   context 'when you arrive at the page' do
+    it 'shows a header that you are on the admin dashboard' do
+      visit "/admin"
+
+      expect(page).to have_content("This is the Admin Dashboard")
+    end
     it 'shows links to admin merchants index' do
       visit "/admin"
 
