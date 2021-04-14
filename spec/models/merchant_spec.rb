1288a1bd8b3fa316e5c1ebbaa4a8c8bbbaa88adf
George Ho <george.h.w.ho@gmail.com>
2021-04-14 13:28:35 -0400

    Add factory bot example

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Merchant, type: :model do
   describe "validations" do
     it {should validate_presence_of :name}
   end
+
+  # describe 'EXAMPLE' do
+  #   before {
+  #     @merchant_1 = create(:random_merchant)
+  #     @merchant_2 = create(:random_merchant)
+  #   }
+
+  #   it 'EXAMPLE' do
+  #     binding.pry
+  #   end
+  # end
 end
