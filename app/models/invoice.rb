223ff8729429a714487b2862230be8458a6300ea
George Ho <george.h.w.ho@gmail.com>
2021-04-14 17:34:09 -0400

    Add ability to find invoices where items are not shipped

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,4 +7,18 @@ class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
 
   enum status: [:"in progress", :cancelled, :completed]
+
+  def self.where_not_successful
+    self.joins(:invoice_items)
+        .where.not("invoice_items.status=2")
+        .order(:created_at)
+        .distinct
+  end
 end
+
+# As an admin,
+# When I visit the admin dashboard
+# Then I see a section for "Incomplete Invoices"
+# In that section I see a list of the ids of all invoices
+# That have items that have not yet been shipped
+# And each invoice id links to that invoice's admin show page
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -3,8 +3,28 @@ require 'rails_helper'
 RSpec.describe Invoice, type: :model do
   describe "relationships" do
     it {should belong_to :customer}
+    it {should have_many :transactions}
     it {should have_many :invoice_items}
     it {should have_many :items}
   end
 
+  describe 'class methods' do
+    describe '.where_not_successful' do
+      it 'returns all the invoices where the items have not been shipped' do
+        invoice = create(:random_invoice)
+        invoice_2 = create(:random_invoice)
+        invoice_3 = create(:random_invoice)
+
+        invoice_item_1 = create(:random_invoice_item, status: 2, invoice: invoice)
+        invoice_item_2 = create(:random_invoice_item, status: 1, invoice: invoice_2)
+        invoice_item_3 = create(:random_invoice_item, status: 0, invoice: invoice_3)
+
+        expect(Invoice.where_not_successful).to eq([invoice_2, invoice_3])
+
+        invoice_item_4 = create(:random_invoice_item, status: 0, invoice: invoice)
+
+        expect(Invoice.where_not_successful).to eq([invoice, invoice_2, invoice_3])
+      end
+    end
+  end
 end
