d5c3c3c5ef4b3f4b32c3bd27a45212aaab86488c
George Ho <george.h.w.ho@gmail.com>
2021-04-14 07:48:46 -0400

    Add admin dashboard link tests

---
+++ spec/features/admin/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'admin index page' do
+  context 'when you arrive at the page' do
+    it 'shows links to admin merchants index' do
+      visit "/admin"
+
+      expect(page).to have_link("Merchant Index")
+    end
+    it 'shows link to admin invoices index' do
+      visit "/admin"
+
+      expect(page).to have_link("Invoice Index")
+    end
+  end
+end
