a78d6183dbae144943dfe9906776dbdfd1cfbfe5
George Ho <george.h.w.ho@gmail.com>
2021-04-15 11:08:36 -0400

    Add functionality for show and update

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -2,4 +2,25 @@ class Admin::InvoicesController < ApplicationController
   def index
     @invoices = Invoice.all
   end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
+
+  def update
+    invoice = Invoice.find(params[:id])
+
+    if invoice.update(invoice_params)
+      redirect_to admin_invoice_path(invoice)
+    elsif
+      redirect_to admin_invoice_path(invoice)
+      flash[:error_explanation] = "Error: #{error_message(invoice.errors)}"
+    end
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:status, :customer_id)
+  end
 end
