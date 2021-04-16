f8b6a545ff9db4c77379b4eaaec2a2fa8467f483
loganjacob76 <nagolisback@gmail.com>
2021-04-15 21:40:00 -0600

    Add enabled & disabled functionality

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'admin merchant index spec' do
   before :each do
-    @merchant_1 = create(:random_merchant)
-    @merchant_2 = create(:random_merchant)
+    @merchant_1 = create(:random_merchant, enabled: true)
+    @merchant_2 = create(:random_merchant, enabled: false)
 
     visit '/admin/merchants'
   end
@@ -12,6 +12,20 @@ RSpec.describe 'admin merchant index spec' do
     it 'i see the names of all the merchants as links to their show page' do
       expect(page).to have_link("#{@merchant_1.name}")
       expect(page).to have_link("#{@merchant_2.name}")
+    end
+
+    context 'when looking at merchants' do
+      it 'shows all enabled & disabled' do
+        within '#enabled_merchants' do
+          expect(page).to have_content(@merchant_1.name)
+          expect(page).to_not have_content(@merchant_2.name)
+        end
+        
+        within '#disabled_merchants' do
+          expect(page).to have_content(@merchant_2.name)
+          expect(page).to_not have_content(@merchant_1.name)
+        end
+      end
     end
   end
 end
