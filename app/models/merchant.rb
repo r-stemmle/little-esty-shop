bd6e3283943a238da9e2c6d1c0f522e961aa60f8
loganjacob76 <nagolisback@gmail.com>
2021-04-16 16:58:22 -0600

    Working on top_merchants method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -12,4 +12,26 @@ class Merchant < ApplicationRecord
   def self.all_disabled
     where(enabled: false)
   end
+
+  def self.top_merchants
+    find_by_sql('SELECT
+      *,
+      SUM ( invoice_items.unit_price * invoice_items.quantity) AS revenue
+    FROM
+      merchants
+      INNER JOIN items ON merchants."id" = items.merchant_id
+      INNER JOIN invoice_items ON items."id" = invoice_items.item_id
+      INNER JOIN invoices ON invoice_items.invoice_id = invoices."id"
+      INNER JOIN transactions ON invoices."id" = transactions.invoice_id 
+    WHERE
+      transactions."result" = 1 
+    GROUP BY
+      merchants."id",
+      items."id",
+      invoice_items."id",
+      invoices."id",
+      transactions."id" 
+    ORDER BY
+      revenue DESC')
+  end
 end
