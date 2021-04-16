c5bdf56af6d22f0a451da9c7b2050409b8379995
loganjacob76 <nagolisback@gmail.com>
2021-04-15 21:39:33 -0600

    Add enabled & disabled queries

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,12 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
 
   validates_presence_of :name
+
+  def self.all_enabled
+    where(enabled: true)
+  end
+
+  def self.all_disabled
+    where(enabled: false)
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,14 +9,18 @@ RSpec.describe Merchant, type: :model do
     it {should validate_presence_of :name}
   end
 
-  # describe 'EXAMPLE' do
-  #   before {
-  #     @merchant_1 = create(:random_merchant)
-  #     @merchant_2 = create(:random_merchant)
-  #   }
+  describe 'class methods' do
+    before do
+      @merchant_1 = create(:random_merchant, enabled: true)
+      @merchant_2 = create(:random_merchant, enabled: false)
+    end
+    
+    it '.all_enabled' do
+      expect(Merchant.all_enabled).to eq([@merchant_1])
+    end
 
-  #   it 'EXAMPLE' do
-  #     binding.pry
-  #   end
-  # end
+    it '.all_disabled' do
+      expect(Merchant.all_disabled).to eq([@merchant_2])
+    end
+  end
 end
