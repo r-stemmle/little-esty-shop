bd615a48ff2514305d441921a272b9ecce06593c
loganjacob76 <nagolisback@gmail.com>
2021-04-15 21:18:05 -0600

    Add enabled column to merchants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_141302) do
+ActiveRecord::Schema.define(version: 2021_04_16_031017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_04_14_141302) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "enabled", default: true
   end
 
   create_table "transactions", force: :cascade do |t|
