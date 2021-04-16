61e9868478167f419e733d72188231bd830be973
loganjacob76 <nagolisback@gmail.com>
2021-04-15 21:03:14 -0600

    Update show spec to follow link from index

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'admin merchant show page' do
-    
+    context 'you are on the admin merchants index page' do
+        it 'merchant name link brings you to show page' do
+            merchant = create(:random_merchant)
+
+            visit '/admin/merchants'
+            
+            click_on "#{merchant.name}"
+            expect(current_path).to eq("/admin/merchants/#{merchant.id}")
+        end
+    end
     context 'you arrive on the page' do
         before :each do
             @merchant = create(:random_merchant)
