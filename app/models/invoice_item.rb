38387c45ee89bf1ff594eb3d64699c09d56fa08d
George Ho <george.h.w.ho@gmail.com>
2021-04-16 23:11:48 -0400

    Add highest revenue merchant ids method

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -9,4 +9,14 @@ class InvoiceItem < ApplicationRecord
   def self.lines_not_shipped
     where('status != ?', 2)
   end
+
+  # this currently returns all merchant_id's by highest revenue
+  def self.highest_revenue_merchant_ids
+    joins(:item, invoice: :transactions)
+    .select("items.merchant_id, sum(invoice_items.unit_price * invoice_items.quantity) as revenue")
+    .where("transactions.result = 1")
+    .group("items.merchant_id")
+    .order(revenue: :desc)
+    .limit(5)
+  end
 end
