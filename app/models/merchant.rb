5512b4066309185020963f8e060cd7420e2f59c0
George Ho <george.h.w.ho@gmail.com>
2021-04-16 23:11:25 -0400

    Remove top merchants from merchant class

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -18,28 +18,6 @@ class Merchant < ApplicationRecord
     where(enabled: false)
   end
 
-  def self.top_merchants
-    find_by_sql('SELECT
-      *,
-      SUM ( invoice_items.unit_price * invoice_items.quantity) AS revenue
-    FROM
-      merchants
-      INNER JOIN items ON merchants."id" = items.merchant_id
-      INNER JOIN invoice_items ON items."id" = invoice_items.item_id
-      INNER JOIN invoices ON invoice_items.invoice_id = invoices."id"
-      INNER JOIN transactions ON invoices."id" = transactions.invoice_id
-    WHERE
-      transactions."result" = 1
-    GROUP BY
-      merchants."id",
-      items."id",
-      invoice_items."id",
-      invoices."id",
-      transactions."id"
-    ORDER BY
-      revenue DESC')
-  end
-
   def ready_to_ship
     items.joins(:invoice_items)
          .joins(:invoices).select('invoice_items.*, items.*, invoices.created_at')
