2f93c65a07c27ab77475590f6c4cf386d143d845
r-stemmle <ryan.stemmle@gmail.com>
2021-04-17 22:51:46 -0400

    Replace missing ends

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -17,5 +17,16 @@ RSpec.describe "Merchant Invoice Show Page" do
         expect(page).to have_content(invoice_1.customer.name)
       end
     end
+
+    it "I see information of the items on the invoice" do
+      merchant = create(:random_merchant)
+      item_1 = create(:random_item, merchant: merchant)
+      invoice_1 = create(:random_invoice)
+      invoice_item_1 = create(:random_invoice_item, item: item_1, invoice: invoice_1)
+
+      visit merchant_invoice_path(merchant, invoice_1)
+
+      within ".show"
+    end
   end
 end
