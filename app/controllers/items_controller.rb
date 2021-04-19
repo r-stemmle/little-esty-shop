25e033bf444379b522bb6881dab9601caf5c63fd
r-stemmle <ryan.stemmle@gmail.com>
2021-04-19 09:27:00 -0400

    Fix dynamic routes by moving controllers into merchant namespace

--- app/controllers/items_controller.rb
+++
@@ -1,6 +0,0 @@
-class ItemsController < ApplicationController
-
-  def index
-    @items = Item.find(params[:merchant_id])
-  end
-end
--- app/controllers/merchants/invoices_controller.rb
+++ app/controllers/merchants/invoices_controller.rb
@@ -1,11 +1,11 @@
 class Merchants::InvoicesController < ApplicationController
   def index
-
     @merchant = Merchant.find(params[:merchant_id])
     @invoice_items = InvoiceItem.by_merchant(@merchant)
   end
 
   def show
+    @merchant = Merchant.find(params[:merchant_id])
     @invoice = Invoice.find(params[:id])
     @invoice_items = InvoiceItem.invoice_items_details(@invoice)
   end
