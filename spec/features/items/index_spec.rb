1d522e5a13b3507174c38b12b9b75613c9532580
Wyattwicks <wyattwicks@gmail.com>
2021-04-19 21:08:50 -0600

    Add testing for final 2 user stories

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -74,7 +74,7 @@ RSpec.describe "Merchant Items Index Page" do
       item_2 = create(:random_item, id: 2, merchant_id: 22)
 
       visit merchant_items_path(merchant)
-      
+
       within '#disabled-items' do
         click_on 'Enable'
         expect(current_path).to eq(merchant_items_path(merchant))
@@ -82,5 +82,39 @@ RSpec.describe "Merchant Items Index Page" do
       end
     end
 
+    it "I see the names of the top 5 most popular items, ranked by total revenue generated" do
+      merchant = create(:random_merchant, id: 22)
+      item_1 = create(:random_item, id: 1, merchant_id: 22)
+      item_2 = create(:random_item, id: 2, merchant_id: 22)
+      item_3 = create(:random_item, id: 3, merchant_id: 22)
+      item_4 = create(:random_item, id: 4, merchant_id: 22)
+      item_5 = create(:random_item, id: 5, merchant_id: 22)
+
+      visit merchant_items_path(merchant)
+
+      expect(page).to have_content("Top Items")
+      within '#top-items' do
+        expect(page).to have_content(item_1.name)
+        expect(page).to have_content(item_2.name)
+        expect(page).to have_content(item_3.name)
+        expect(page).to have_content(item_4.name)
+        expect(page).to have_content(item_5.name)
+
+        expect(page).to have_content(item_1.revenue)
+        expect(page).to have_content(item_2.revenue)
+        expect(page).to have_content(item_3.revenue)
+        expect(page).to have_content(item_4.revenue)
+        expect(page).to have_content(item_5.revenue)
+
+        expect(page).to have_content(item_1.top_day)
+        expect(page).to have_content(item_2.top_day)
+        expect(page).to have_content(item_3.top_day)
+        expect(page).to have_content(item_4.top_day)
+        expect(page).to have_content(item_5.top_day)
+        #I was thinking the .top_day method would be for the last user story, and
+        #the .revenue method would be used to calculate the item's sales.
+        #there are tests for these in the item model spec, if you want to change these names feel free!
+      end
+    end
   end
 end
