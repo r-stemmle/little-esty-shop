a80e0bb5a8aca9a87537adb1d0756b6b7bf273a3
loganjacob76 <nagolisback@gmail.com>
2021-04-19 18:44:41 -0600

    Update methods to find commit # by author

--- app/services/git_hub_service.rb
+++ app/services/git_hub_service.rb
@@ -1,5 +1,4 @@
 class GitHubService
-  # SRP: Communicate with Github API
     def initialize(token)
       @token = token
     end
@@ -11,32 +10,25 @@ class GitHubService
           'Authorization' => "token #{@token}",
           'Accept' => 'application/vnd.github.v3+json'
         }
-      ) # Set up a "connection" object
+      )
+    end
+
+    def commits_by_author(author)
+      get_commits.find do |data|
+        data[:author][:login] == author
+      end[:total]
     end
 
     def get_commits
-      resp = conn.get('/repos/georgehwho/little-esty-shop/commits')
+      resp = conn.get('/repos/georgehwho/little-esty-shop/stats/contributors', {
+        owner: 'georgehwho',
+        repo: 'little-esty-shop'
+      })
       JSON.parse(resp.body, symbolize_names: true)
     end
 
     def get_collaborators
       resp = conn.get('/repos/georgehwho/little-esty-shop/collaborators')
       JSON.parse(resp.body, symbolize_names: true)
-    end
-
-    def get_followers
-      resp = conn.get('/user/followers') # sends a request
-      JSON.parse(resp.body, symbolize_names: true)
-    end
-
-    def get_followings
-      resp = conn.get('/user/following') # sends a request
-      JSON.parse(resp.body, symbolize_names: true)
-    end
-
-    def follow(username)
-      resp = conn.put("/user/following/#{username}") do |req|
-        req.headers['Content-Length'] = 0
-      end
     end
   end
