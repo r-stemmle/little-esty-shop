2dd0ac1a6803e2dacf85f67aea683dde067c05ef
r-stemmle <ryan.stemmle@gmail.com>
2021-04-23 22:28:59 -0400

    Add update and edit methods to discounts controller

--- app/controllers/merchants/discounts_controller.rb
+++ app/controllers/merchants/discounts_controller.rb
@@ -26,13 +26,28 @@ class Merchants::DiscountsController < ApplicationController
     end
   end
 
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.find(params[:id])
+  end
+
+  def update
+    merchant = Merchant.find(params[:merchant_id])
+    discount = Discount.find(params[:id])
+    if discount.update(discount_params)
+      redirect_to merchant_discount_path(merchant, discount)
+    else
+      redirect_to edit_merchant_discount_path(merchant, discount)
+      flash[:error] = "Error: #{error_message(discount.errors)}"
+    end
+  end
+
   def destroy
     merchant = Merchant.find(params[:merchant_id])
     discount = Discount.find(params[:id])
     discount.destroy
     redirect_to merchant_discounts_path(merchant)
   end
-
 
   private
 
