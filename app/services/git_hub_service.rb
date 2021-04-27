6c8f362c8d2767f6bb4faa3d3bc2a3e63261c2a8
r-stemmle <ryan.stemmle@gmail.com>
2021-04-27 16:29:59 -0400

    Uncomment and refactor

--- app/services/git_hub_service.rb
+++ app/services/git_hub_service.rb
@@ -1,56 +1,53 @@
 require 'faraday'
 require 'json'
+
 class GitHubService
-    def initialize(token)
-      @token = token
-    end
-
-    def conn
-      Faraday.new(
-        url: 'https://api.github.com',
-        headers: {
-          'Authorization' => "token #{@token}",
-          'Accept' => 'application/vnd.github.v3+json'
-        }
-      )
-    end
-
-    def commits_by_author(commits, author)
-      commits.find do |data|
-        data[:author][:login] == author
-      end[:total]
-    end
-
-    def get_commits
-      resp = conn.get('/repos/georgehwho/little-esty-shop/stats/contributors', {
-        owner: 'georgehwho',
-        repo: 'little-esty-shop'
-      })
-      JSON.parse(resp.body, symbolize_names: true)
-    end
-
-    def get_collaborators
-      resp = conn.get('/repos/georgehwho/little-esty-shop/collaborators')
-      JSON.parse(resp.body, symbolize_names: true)
-    end
-
-    def get_pull_requests
-      resp = conn.get('/repos/georgehwho/little-esty-shop/pulls', {
-        owner: 'georgehwho',
-        repo: 'little-esty-shop',
-        state: 'closed',
-        sort: 'is merged'
-        })
-        json = JSON.parse(resp.body, symbolize_names: true)
-        # json.select { |pr| pr[:merged_at] }
-    end
-
-    def get_repo_name
-      resp = conn.get('/repos/georgehwho/little-esty-shop')
-      json = JSON.parse(resp.body, symbolize_names: true)
-      json[:name]
-    end
+  def initialize(token)
+    @token = token
   end
 
-  service = GitHubService.new(ENV["GITHUB_TOKEN"])
-  service.get_pull_requests
+  def conn
+    Faraday.new(
+      url: 'https://api.github.com',
+      headers: {
+        'Authorization' => "token #{@token}",
+        'Accept' => 'application/vnd.github.v3+json'
+      }
+    )
+  end
+
+  def commits_by_author(commits, author)
+    commits.find do |data|
+      data[:author][:login] == author
+    end[:total]
+  end
+
+  def get_commits
+    resp = conn.get('/repos/georgehwho/little-esty-shop/stats/contributors', {
+      owner: 'georgehwho',
+      repo: 'little-esty-shop'
+    })
+    JSON.parse(resp.body, symbolize_names: true)
+  end
+
+  def get_collaborators
+    resp = conn.get('/repos/georgehwho/little-esty-shop/collaborators')
+    JSON.parse(resp.body, symbolize_names: true)
+  end
+
+  def get_pull_requests
+    resp = conn.get('/repos/georgehwho/little-esty-shop/pulls', {
+      owner: 'georgehwho',
+      repo: 'little-esty-shop',
+      state: 'closed'
+      })
+      json = JSON.parse(resp.body, symbolize_names: true)
+      json.select { |pr| pr[:merged_at] }
+  end
+
+  def get_repo_name
+    resp = conn.get('/repos/georgehwho/little-esty-shop')
+    json = JSON.parse(resp.body, symbolize_names: true)
+    json[:name]
+  end
+end
