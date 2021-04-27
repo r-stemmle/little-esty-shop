573b6e8c28e64d230f87d032b408a680b8a50ffd
r-stemmle <ryan.stemmle@gmail.com>
2021-04-27 14:07:07 -0400

    Add has many items to discount

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,3 +1,8 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
+  has_many :items, through: :merchant
+
+  def self.find_by_min_ordered(needed)
+    where("quantity <= #{needed}").order(quantity: :desc).limit(1).first
+  end
 end
